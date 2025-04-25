2b685f8936a515b3a05b26de2201c0689b8af963
scorchedrice <wldnd2977@gmail.com>
2025-04-26 01:12:13 +0900

    🐛 fix: 렌더링 시 localStorage 확인 로직 변경 => 깜빡이는 이슈 해결

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,7 +1,11 @@
 import "./src/global.css";
 import "prismjs/themes/prism-solarizedlight.css"
 
-// gatsby-ssr.js 또는 gatsby-browser.js
 export const onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ className: 'dark' }) // 기본 다크모드
+  const theme = localStorage.getItem("theme");
+  if (theme) {
+    setHtmlAttributes({ className: theme });
+  } else {
+    setHtmlAttributes({ className: 'dark' }) // 기본 다크모드
+  }
 }
