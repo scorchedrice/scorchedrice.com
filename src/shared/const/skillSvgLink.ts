a185d97f3c172b8ff08d9835ae0759b6cd5583a6
scorchedrice <wldnd2977@gmail.com>
2025-05-18 13:13:46 +0900

    💡update: 프로필, 프로젝트 설명 수정

--- src/shared/const/skillSvgLink.ts
+++ src/shared/const/skillSvgLink.ts
@@ -2,16 +2,14 @@ const skillSvgLinks = {
   frontend: [
     { name: "React", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
     { name: "Next.js", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" },
+    { name: "Gatsby", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gatsby/gatsby-original.svg"},
+    { name: "Electron", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/electron/electron-original.svg"},
     { name: "Flutter", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg" },
     { name: "Tailwind", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"},
-    { name: "Electron", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/electron/electron-original.svg"},
-    { name: "Gatsby", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gatsby/gatsby-original.svg"},
   ],
   backend: [
     { name: "Nest.js", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg"},
-    { name: "Postgres", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" },
-    { name: "MongoDB", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"},
-    { name: "Mongoose", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg" }
+    { name: "Bolt", Url: "https://a.slack-edge.com/58efd/img/api/bolt/bolt-logo.svg" }
   ],
   language: [
     { name: "HTML5", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" },
@@ -21,8 +19,8 @@ const skillSvgLinks = {
     { name: "Dart", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dart/dart-original.svg" },
     { name: "Python", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" }
   ],
-  test: [
-    { name: "Jest", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg" }
+  tool: [
+    { name: "Figma", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" },
   ]
 };
 
@@ -30,5 +28,5 @@ export const skillSvgLinkList = [
   { name: "Language", data: skillSvgLinks.language },
   { name: "Frontend", data: skillSvgLinks.frontend },
   { name: "Backend", data: skillSvgLinks.backend },
-  { name: "Testing & Tools", data: skillSvgLinks.test }
+  { name: "Tool", data: skillSvgLinks.tool }
 ];
