415b02ae0c47c9864e25e48890380ba2f6fb8c17
scorchedrice <wldnd2977@gmail.com>
2025-06-26 10:29:27 +0900

    💡update: 이력서 backend 섹션, frontend - expo

--- src/shared/const/skillSvgLink.ts
+++ src/shared/const/skillSvgLink.ts
@@ -1,15 +1,20 @@
+import ExpoUrl from "../assets/profile/logo-type-a.png";
+
 const skillSvgLinks = {
   frontend: [
     { name: "React", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
     { name: "Next.js", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" },
     { name: "Gatsby", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gatsby/gatsby-original.svg"},
     { name: "Electron", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/electron/electron-original.svg"},
+    { name: "Expo", Url: ExpoUrl},
     { name: "Flutter", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg" },
-    { name: "Tailwind", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"},
+    { name: "Tailwindcss", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"},
   ],
-  backend: [
+  "backend & infra": [
     { name: "Nest.js", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg"},
-    { name: "Bolt", Url: "https://a.slack-edge.com/58efd/img/api/bolt/bolt-logo.svg" }
+    { name: "Express", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"},
+    { name: "Bolt", Url: "https://a.slack-edge.com/58efd/img/api/bolt/bolt-logo.svg" },
+    { name: "Docker", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" }
   ],
   language: [
     { name: "HTML5", Url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" },
@@ -27,6 +32,6 @@ const skillSvgLinks = {
 export const skillSvgLinkList = [
   { name: "Language", data: skillSvgLinks.language },
   { name: "Frontend", data: skillSvgLinks.frontend },
-  { name: "Backend", data: skillSvgLinks.backend },
+  { name: "Backend & Infra", data: skillSvgLinks["backend & infra"] },
   { name: "Tool", data: skillSvgLinks.tool }
 ];
